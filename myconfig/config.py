7af336dd1f6c7661e81ac55f1983c3cfb4f4eb1e
cl2227619761 <cl2227619761@gmail.com>
2019-06-09 16:08:35 +0800

    配置文件

--- myconfig/config.py
+++ myconfig/config.py
@@ -1,14 +1,16 @@
 """
 本脚本是配置文件所在的脚本
 """
-from pprint import pprint
+#  from pprint import pprint
 
 
 class Config:
     """配置文件类"""
     # 数据集相关的配置
-    root = "/home/caolei/ALL_data/VOC2007/"
+    root = "/home/caolei/code/ALL_data/VOC2007/"
     image_set = "ALL_train"
+    # 数据集标签类别
+    box_label_names = ("正常", "异常")
 
 
 OPT = Config()
