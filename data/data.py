0d3becb4cabf86069cb0584d3a803a40efc8de10
cl2227619761 <cl2227619761@gmail.com>
2019-06-09 16:08:06 +0800

    完成了数据集的准备

--- data/data.py
+++ data/data.py
@@ -1,16 +1,23 @@
 """
-本脚本是关于数据集的生成，用于训练和验证
+本脚本是关于数据集的准备
 """
 import os
 import sys
-sys.path.append("../")
+import collections
+import xml.etree.ElementTree as ET
 
+import torch
 from torchvision.datasets import VisionDataset
+from PIL import Image
 
-from myconfig.config import OPT 
+sys.path.append("../")
+try:
+    from myconfig.config import OPT
+except Exception:
+    raise
 
 
-# 定义数据集，目标检测的数据集的准备类似于普通的CNN，也是要继承torch.utils.
+# 定义数据集，目标检测的数据集的准备类似于普通的CNN
 # .data的, 里面的__len__和__getitem__方法
 class ALLDetection(VisionDataset):
     """仿写源码里面的VOCDetection"""
@@ -24,17 +31,18 @@ class ALLDetection(VisionDataset):
         """
         参数：
             root: Dataset的根目录，例如././VOC2007/
-            image_set: 要使用的数据集的名称, 比如train, trainval, val
+            image_set: 要使用的数据集的名称比如train, trainval
             transform: 函数，接受PIL image, 返回处理后的对象
             target_transform: 函数，对target作相应的处理
         """
         # 继承了VisionDataset, 具体参看VisionDataset源码(TODO)
-        super(ALLDetection, self).__init__(root, transforms,
-                                           transform, target_transform)
+        super(ALLDetection, self).__init__(root, transforms, transform,
+                                           target_transform)
         self.image_set = image_set
-        image_dir = os.path.join(root, "JPEGImages")  # 图像所在的文件夹
-        annotation_dir = os.path.join(root, "Annotations")  # 标注文件夹
-
+        # 图像所在的文件夹
+        image_dir = os.path.join(root, "JPEGImages")
+        # 标注文件夹
+        annotation_dir = os.path.join(root, "Annotations")
         if not os.path.isdir(root):
             raise RuntimeError("未发现数据所在目录，请检查!")
 
@@ -50,17 +58,81 @@ class ALLDetection(VisionDataset):
         if filenames[0].endswith(".jpg"):
             filenames = [x.strip(".jpg") for x in filenames]
 
+        # 图像所在的路径构成的列表
         self.images = [os.path.join(image_dir, x + ".jpg") for x in filenames]
-        self.annotations = [os.path.join(annotation_dir, x + ".xml")
-                            for x in filenames]
+        # 图像对应的标注文件的路径所在的列表
+        self.annotations = [
+            os.path.join(annotation_dir, x + ".xml") for x in filenames
+        ]
         assert (len(self.images) == len(self.annotations))
+
+    def __getitem__(self, index):
+        """
+        参数：
+            index: 图像的索引
+        返回：
+            (image, target)组成的元组
+        """
+        img = Image.open(self.images[index]).convert("RGB")
+        pre_target = self.parse_xml(
+            ET.parse(self.annotations[index]).getroot())
+        objs = pre_target["annotation"]["object"]
+        num_objs = len(objs)
+        boxes = []
+        labels = []
+        for i in range(num_objs):
+            xmin = float(objs[i]["bndbox"]["xmin"])
+            ymin = float(objs[i]["bndbox"]["ymin"])
+            xmax = float(objs[i]["bndbox"]["xmax"])
+            ymax = float(objs[i]["bndbox"]["ymax"])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+            name = objs[i]["name"]
+            labels.append(OPT.box_label_names.index(name))
+
+        # 转为torch张量
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.images)
+
+    def parse_xml(self, node):
+        """解析xml文件，返回xml文件内容到字典中"""
+        ALL_dict = {}
+        children = list(node)
+        if children:
+            def_dic = collections.defaultdict(list)
+            for dc in map(self.parse_xml, children):
+                for ind, v in dc.items():
+                    def_dic[ind].append(v)
+            ALL_dict = {
+                node.tag:
+                    {ind: v[0] if len(v) == 1 else v
+                     for ind, v in def_dic.items()}
+            }
+        if node.text:
+            text = node.text.strip()
+            if not children:
+                ALL_dict[node.tag] = text
+        return ALL_dict
 
 
 def main():
     """调试用"""
     dataset = ALLDetection()
+    img, target = dataset[0]
+    print(img)
+    print(target)
 
 
 if __name__ == "__main__":
     main()
-
