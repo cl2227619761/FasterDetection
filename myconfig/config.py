699ba5465902b99175c829a3c92f1b0b07b69056
luyiyun <18845728185@163.com>
2019-06-10 10:03:27 +0800

    update

--- myconfig/config.py
+++ myconfig/config.py
@@ -7,7 +7,8 @@
 class Config:
     """配置文件类"""
     # 数据集相关的配置
-    root = "/home/caolei/code/ALL_data/VOC2007/"
+    # root = "/home/caolei/code/ALL_data/VOC2007/"
+    root = "/home/dl/code/caolei/ALL_data/VOC2007/"
     image_set = "ALL_train"
     # 数据集标签类别
     box_label_names = ("正常", "异常")
